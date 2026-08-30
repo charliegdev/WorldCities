d5668ad00710294233a2d2b8a2e836f69cac5bcc
charliegdev <charliegdev@gmail.com>
2026-08-30 10:53:56 -0700

    Add free community license.

--- worldcities.client/src/app/app.config.ts
+++ worldcities.client/src/app/app.config.ts
@@ -13,6 +13,8 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     providePrimeNG({
       theme: { preset: Aura, options: { darkModeSelector: false } },
+      license:
+        'eyJpZCI6ImM4MDUxOWI1LWNlOGQtNDM3Yi04OTQxLTc1YTdlMmUzMjQ0MSIsInByb2R1Y3QiOiJwcmltZXVpIiwidGllciI6ImNvbW11bml0eSIsInR5cGUiOiJkZXYiLCJpYXQiOjE3ODgxMTIzNDgsImV4cCI6MTgxOTY0ODM0OH0.ltiiRt2MPh7lryrQh3Onpv6yI3gjLlin62g07MDIUsUOBUHi7VWe6ErBOTz2YvEjGEZm3rgmNFrbTXOBJ5XHDA',
     }),
   ],
 };
