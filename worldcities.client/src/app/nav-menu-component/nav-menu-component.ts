15808e5efcb0888756e3e0cf026b5b830090d6a5
charliegdev <charliegdev@gmail.com>
2026-08-30 10:08:56 -0700

    Add PrimeNG, and migrate from css to scss

--- worldcities.client/src/app/nav-menu-component/nav-menu-component.ts
+++ worldcities.client/src/app/nav-menu-component/nav-menu-component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   imports: [],
   selector: 'app-nav-menu-component',
-  styleUrl: './nav-menu-component.css',
+  styleUrl: './nav-menu-component.scss',
   templateUrl: './nav-menu-component.html',
 })
 export class NavMenuComponent {}
