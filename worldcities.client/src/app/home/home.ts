b4d85fdbccd740ff88513f642002480b22b6e320
charliegdev <charliegdev@gmail.com>
2026-09-03 22:07:42 -0700

    Use NgOptimizedImage

--- worldcities.client/src/app/home/home.ts
+++ worldcities.client/src/app/home/home.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
-  imports: [],
+  imports: [NgOptimizedImage],
   selector: 'app-home',
   styleUrl: './home.scss',
   templateUrl: './home.html',
